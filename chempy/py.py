c13da8e4ed041f7266f9283cc2c6d95ec2ae9249
git <git@piserv.local>
2026-09-16 16:37:59 -0400

    add license information

--- chempy/py.py
+++ chempy/py.py
@@ -1,6 +1,21 @@
+# This file is part of ChemPy.
+# Copyright (C) 2026 Chem
+#
+# ChemPy is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ChemPy is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with ChemPy. If not, see <http://www.gnu.org/licenses/>.
+
 import os
-from chempy import files
-#from chempy.files import parent_path, file_name, file_extension, file_safe_write
+from chempy.files import parent_path, file_name, file_extension, file_safe_write
 
 
 def detect_indentation(path:str) -> int:
