2170732e2ac67b94e9383a97d247e563750682a8
git <git@piserv.local>
2026-09-15 23:50:28 -0400

    increment chempy version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="chempy",
-    version="2.1.1",
+    version="2.1.2",
     packages=find_packages(),
     install_requires=[],
     author="Chem",
